9f407e874b7bafc0cefba656c563d37421b8e984
Andreas-Huning <148219563+Andreas-Huning@users.noreply.github.com>
2024-02-21 18:45:24 +0100

    Bilder in public verschoben

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
+
 function TableRow(props) {
     // console.log(props.listItem);
     return (
         <tr>       
             <td>{props.index}</td> 
             <td>{props.listItem}</td> 
-            <td onClick={()=>{props.editToDo(props.index)}}><img src="/src/assets/edit.png" alt="Löschen" className="delEditImage" /></td>
-            <td onClick={()=>{props.delToDo(props.index)}}><img src="/src/assets/del.png" alt="Löschen" className="delEditImage" /></td>       
+            <td onClick={()=>{props.editToDo(props.index)}}><img src="img/edit.png" alt="Editieren" className="delEditImage" /></td>
+            <td onClick={()=>{props.delToDo(props.index)}}><img src="img/del.png" alt="Löschen" className="delEditImage" /></td>       
         </tr>
     );
 }
